6bfee3af6460a1cde09a282e04612fad3dd5774a
ashlytorres <104461184+ashlytorres@users.noreply.github.com>
2022-04-28 12:42:13 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("button").click(function() {
+    let storiesCreated = $(".stories-ticker").val();
+    let name = $(".name-input").val();
+    let transport = $(".trasport-inout").val();
+    let Adjective = $(".adjective-input").val(); 
+    let number = $(".number-input").val();
+    //Write code below!
+     $(".story").append("One day my friend _________ and I decided to take a trip to the beach, so we packed our bags and took the _________ over.It was a __________ day at the beach! We ended up staying for _________ hours!");
+
+});
+
